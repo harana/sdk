539d39bfd137031ceeffc66a720c7d9d6da7f8ac
Naden <naden@harana.com>
2024-07-09 15:04:24 +1000

    Updates

--- shared/src/main/scala/com/harana/sdk/shared/models/flow/ActionTypeInfo.scala
+++ shared/src/main/scala/com/harana/sdk/shared/models/flow/ActionTypeInfo.scala
@@ -4,11 +4,15 @@ import com.harana.sdk.shared.models.common.ParameterGroup
 import com.harana.sdk.shared.models.flow.actiontypes.ActionTypeGroup
 
 trait ActionTypeInfo {
-  val id: String
+  val id: ActionTypeInfo.Id
   val tags: Set[String]
   val group: ActionTypeGroup
   val inputPorts: List[Port]
   val outputPorts: List[Port]
   val parameterGroups: List[ParameterGroup]
   val mode: ActionMode = ActionMode.Normal
+}
+
+object ActionTypeInfo {
+  type Id = String
 }
