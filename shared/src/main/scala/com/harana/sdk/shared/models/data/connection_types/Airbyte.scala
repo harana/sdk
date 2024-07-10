c444f6025549d0b751746b553e1a1213ef4d2b56
Naden <naden@harana.com>
2024-07-10 14:37:00 +1000

    Updates

--- shared/src/main/scala/com/harana/sdk/shared/models/data/connection_types/Airbyte.scala
+++ shared/src/main/scala/com/harana/sdk/shared/models/data/connection_types/Airbyte.scala
@@ -1,9 +1,11 @@
 package com.harana.sdk.shared.models.data.connection_types
 
-import com.harana.sdk.shared.models.common.{Parameter, ParameterGroup, ParameterValue}
+import com.harana.sdk.shared.models.common.ParameterGroup
 import com.harana.sdk.shared.models.data.ConnectionType.ConnectionTypeId
 import com.harana.sdk.shared.models.data.{ConnectionType, SyncDirection}
+import io.circe.generic.JsonCodec
 
+@JsonCodec
 case class Airbyte(id: ConnectionTypeId,
                    name: String,
                    parameterGroups: List[ParameterGroup],
